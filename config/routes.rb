e5dcbe109c7e7af4b09217dd458388304d33d01d
Dan Gutt <dgutt10@gmail.com>
2018-09-15 15:53:33 -0500

    signup route url changed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pages
