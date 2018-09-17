1bf95f9b8096c2227e4f70f5fae5d67ff3b42c4e
Dan Gutt <dgutt10@gmail.com>
2018-09-17 07:51:00 -0500

    created tests for associations in category and posts model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'has_many posts' do
+      association = described_class.reflect_on_association(:posts)
+      expect(association).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'belongs_to userf' do
+      association = described_class.reflect_on_association(:user).macro
+      expect(association).to eq :belongs_to
+    end
+    
+    it 'belongs_to category' do
+      assocation = described_class.reflect_on_association(:category).macro
+      expect(assocation).to eq :belongs_to
+    end
+  end
 end
