2ec4f6c99fbc15868aa7e02bbeb66ee88d2a6f65
Dan Gutt <dgutt10@gmail.com>
2018-09-17 10:38:59 -0500

    Posts can be displayed on the home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
   end
+
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :pages
   root to: "pages#index"
