1f675571a28b8f58a833d1bdb35ff67b0ffc5245
Dan Gutt <dgutt10@gmail.com>
2018-09-17 15:39:01 -0500

    added tests for post_format_partial_path helper

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -59,4 +59,19 @@ RSpec.describe PostsHelper, type: :helper do
     end
   end
 
+  context "#post_format_partial_path" do
+    it "returns the home_page partial's path" do
+      helper.stub(:current_page?).and_return(true)
+      expect(helper.post_format_partial_path). to(
+        eq 'posts/post/home_page'
+      )
+    end
+    it "returns the branch_page partial's path" do
+      helper.stub(:current_page?).and_return(false)
+      expect(helper.post_format_partial_path). to(
+        eq 'posts/post/branch_page'
+      )
+    end
+  end
+
 end
