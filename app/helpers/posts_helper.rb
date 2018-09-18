ad1b9b1a2d75a4f03d1914fa5aca683dabda5ea3
Dan Gutt <dgutt10@gmail.com>
2018-09-18 15:44:40 -0500

    tests for posts_for_service class

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -16,7 +16,7 @@ module PostsHelper
     end
 
     def no_posts_partial_path(posts)
-        posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+        posts.empty? ? 'shared/no_posts' : 'shared/empty_partial'
     end
 
     def post_format_partial_path
--- app/services/posts_for_branch_service.rb
+++ app/services/posts_for_branch_service.rb
@@ -1,19 +1,19 @@
 class PostsForBranchService 
-    def intialize(params)
-        branch = params[:action]
-        search = params[:search]
-        category = params[:category]
+    def initialize(params)
+        @branch = params[:branch]
+        @search = params[:search]
+        @category = params[:category]
     end
 
     def call
-        if category.blank? && search.blank?
-            posts = Post.by_branch(branch).all
-        elsif category.blank? && search.present?
-            posts = Post.by_branch(branch).search(search)
-        elsif category.present? && search.blank?
-            posts = Post.by_category(branch, category)
-        elsif category.present? && search.present?
-            posts = Post.by_category(branch, category).search(search)
+        if @category.blank? && @search.blank?
+            posts = Post.by_branch(@branch).all
+        elsif @category.blank? && @search.present?
+            posts = Post.by_branch(@branch).search(@search)
+        elsif @category.present? && @search.blank?
+            posts = Post.by_category(@branch, @category)
+        elsif @category.present? && @search.present?
+            posts = Post.by_category(@branch, @category).search(@search)
         end
     end
 
