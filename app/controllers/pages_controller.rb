5261b699a6147ddeff71cce166ba0d4cbe1dcf55
Dan Gutt <dgutt10@gmail.com>
2018-09-19 09:28:07 -0500

    Add tests for posting new posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
     def index
-        @hobby_posts = Post.limit(8)
-        @study_posts = Post.limit(8)
-        @team_posts = Post.limit(8)
+        @hobby_posts = Post.by_branch('hobby').limit(8)
+        @study_posts = Post.by_branch('study').limit(8)
+        @team_posts = Post.by_branch('team').limit(8)
     end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :redirect_if_not_signed_in, only: [:new]
+
     def index
     end
 
